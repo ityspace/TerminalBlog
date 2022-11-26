02b66c4bd4e90ed7c2079feced4de7e63e7f9291
Your Name <you@example.com>
2022-11-26 18:51:26 +0800

    first commit

--- src/script/write.sh
+++ src/script/write.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "File name (with .txt): " File
+read -p "File name (no suffix): " File
 read -p "Title: " Title
 read -p "Date: " Date
 
@@ -10,29 +10,29 @@ if [ -z "$Title"  ] || [ -z "$File"  ] || [ -z "$Date"  ]; then
 	exit 0
 fi
 
-test -e posts/$File && echo -n "\033[31mError: File is exist!\033[0m" && exit 0
+test -e posts/"$File".txt && echo -n "\033[31mError: File is exist!\033[0m" && exit 0
 
 echo
 read -p "Editor: " Editor
 
-touch posts/$File
+touch posts/"$File".txt
 
 echo -n "\033[33mOpening ... \033[0m"
 sleep 1
 echo
 
-$Editor "posts/$File"
+$Editor "posts/"$File".txt"
 
-if [ -s posts/$File ]; then 
-	sed -i '1i'$Date posts/$File
-	sed -i '1a'$Title posts/$File
-	echo >> posts/$File
+if [ -s posts/"$File".txt ]; then 
+	sed -i '1i'$Date posts/"$File".txt
+	sed -i '1a'$Title posts/"$File".txt
+	echo >> posts/"$File".txt
 	echo
 	sleep 1s
 	echo Success!
 	exit 0
 else
-	echo -n "\033[33mWarning: File is empty\033[0m" && rm -rf posts/$File
+	echo -n "\033[33mWarning: File is empty\033[0m" && rm -rf posts/"$File".txt
 	exit 0
 fi
 
